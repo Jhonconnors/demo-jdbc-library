48fcfc1cb671fa37a4775deb48b1b6e31d04f681
Jhonconnors <anonimat.jk@gmail.com>
2024-03-18 00:33:50 -0300

    add BasicDataSource and quit Hikari

--- src/main/java/com/example/demo/library/config/CustomDataSourceConfig.java
+++ src/main/java/com/example/demo/library/config/CustomDataSourceConfig.java
@@ -1,8 +1,10 @@
 package com.example.demo.library.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +12,19 @@ import javax.sql.DataSource;
 
 
 @Configuration
-@ConditionalOnProperty(name = "app.datasource.custom.enable", havingValue = "DATABASE", matchIfMissing = false)
+//@ConditionalOnProperty(name = "app.datasource.custom.enable", havingValue = "DATABASE", matchIfMissing = false)
 public class CustomDataSourceConfig {
+
+    @Value("${app.datasource.custom.enable}")
+    private String enable;
 
     @Bean
     @ConfigurationProperties(prefix = "app.datasource.custom") // Prefijo para las propiedades de HikariCP
     public DataSource dataSource(){
-        return new BasicDataSource();
+        if (enable.equals("DATABASE")){
+            return new BasicDataSource();
+        }
+        return DataSourceBuilder.create().build();
     }
 
 }
