2c2c9fb77ff89e1f3f36fe5a02e455ca7c842b65
Jhonconnors <anonimat.jk@gmail.com>
2024-03-18 16:52:53 -0300

    delete bean and add simple method by DataSource

--- src/main/java/com/example/demo/library/persistence/ErrorPersistenceManagement.java
+++ src/main/java/com/example/demo/library/persistence/ErrorPersistenceManagement.java
@@ -1,45 +1,39 @@
 package com.example.demo.library.persistence;
 
+
 import com.example.demo.library.model.EntityError;
+import jakarta.annotation.PostConstruct;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.sql.DataSource;
-
 
 @Component
 public class ErrorPersistenceManagement {
 
-    private final JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
 
     @Value("${app.datasource.custom.enable:TOPIC}")
     private String enable;
 
-    public ErrorPersistenceManagement(){
+    @PostConstruct
+    public void init(){
         if (enable.equals("DATABASE")){
             this.jdbcTemplate = new JdbcTemplate(dataSource());
         } else {
             this.jdbcTemplate = null;
         }
-
     }
-
-    //@ConfigurationProperties(prefix = "app.datasource.custom") // Prefijo para las propiedades de HikariCP
-    public DataSource dataSource(){
-        if (enable.equals("DATABASE")){
-            return new BasicDataSource();
-        }
-        return DataSourceBuilder.create().build();
-    }
-
     public void insertErrorToDatabase(String table, EntityError error){
         String sql = "INSERT INTO "+ table+"(name, errorType, date) VALUES(?, ?, ?)";
         jdbcTemplate.update(sql, error.getName(), error.getErrorType(), error.getDate() );
+    }
+
+    @ConfigurationProperties(prefix = "app.datasource.custom") // Prefijo para las propiedades de HikariCP
+    public DataSource dataSource(){
+        return new BasicDataSource();
     }
 }
