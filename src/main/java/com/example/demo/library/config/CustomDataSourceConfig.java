d763dfe443b9cdf525b7f865fe4185a9fd4d6c52
Jhonconnors <anonimat.jk@gmail.com>
2024-03-17 23:44:02 -0300

    add to DataSOurce with If

--- src/main/java/com/example/demo/library/config/CustomDataSourceConfig.java
+++ src/main/java/com/example/demo/library/config/CustomDataSourceConfig.java
@@ -2,20 +2,31 @@ package com.example.demo.library.config;
 
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 
 @Configuration
-@ConditionalOnProperty(name = "app.datasource.custom.enable", havingValue = "true", matchIfMissing = false)
+@ConditionalOnProperty(name = "app.datasource.custom.enable", havingValue = "DATABASE", matchIfMissing = false)
 public class CustomDataSourceConfig {
+
+    @Value("${app.datasource.custom.enable:#{null}}")
+    private String enable;
 
     @Bean
     @ConfigurationProperties(prefix = "app.datasource.custom") // Prefijo para las propiedades de HikariCP
-    public HikariDataSource dataSource(){
-        return new HikariDataSource();
+    public DataSource dataSource(){
+        if (enable.equals("DATABASE")){
+            return new HikariDataSource();
+        }
+        return DataSourceBuilder.create().build();
+
     }
 
 }
