d9e3307455d597349f7fc4e0de5294cf443ba6b6
Jhonconnors <anonimat.jk@gmail.com>
2024-03-18 21:34:55 -0300

    delete ConditionalOnProperty and dimplify init method

--- src/main/java/com/example/demo/library/persistence/ErrorPersistenceManagement.java
+++ src/main/java/com/example/demo/library/persistence/ErrorPersistenceManagement.java
@@ -1,38 +1,21 @@
 package com.example.demo.library.persistence;
 
-
 import com.example.demo.library.model.EntityError;
 import jakarta.annotation.PostConstruct;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 @Component
-@ConditionalOnProperty(name = "app.datasource.custom.enable", havingValue = "DATABASE",
-        matchIfMissing = false)
 public class ErrorPersistenceManagement {
 
     private JdbcTemplate jdbcTemplate;
 
     @Value("${app.datasource.custom.enable:TOPIC}")
     private String enable;
-
-    @PostConstruct
-    public void init(){
-        if (enable.equals("DATABASE")){
-            this.jdbcTemplate = new JdbcTemplate(dataSource());
-        } else {
-            this.jdbcTemplate = null;
-        }
-    }
-    public void insertErrorToDatabase(String table, EntityError error){
-        String sql = "INSERT INTO "+ table+"(name, errorType, date) VALUES(?, ?, ?)";
-        jdbcTemplate.update(sql, error.getName(), error.getErrorType(), error.getDate() );
-    }
 
     @Value("${app.custom.datasource.url:#{null}}")
     private String url;
@@ -55,19 +38,29 @@ public class ErrorPersistenceManagement {
     @Value("${app.custom.datasource.maxWaitMillis:3000}")
     private long maxWaitMillis;
 
+    @PostConstruct
+    public void init(){
+        if (enable.equals("DATABASE") && (username !=null || url != null ||
+                password != null || driver != null)){
+            this.jdbcTemplate = new JdbcTemplate(dataSource());
+        } else {
+            this.jdbcTemplate = null;
+        }
+    }
+    public void insertErrorToDatabase(String table, EntityError error){
+        String sql = "INSERT INTO "+ table+"(name, errorType, date) VALUES(?, ?, ?)";
+        jdbcTemplate.update(sql, error.getName(), error.getErrorType(), error.getDate() );
+    }
+
     public DataSource dataSource(){
         BasicDataSource dataSource = new BasicDataSource();
-        if (username !=null || url != null ||
-                password != null || driver != null){
-            dataSource.setUrl(this.url);
-            dataSource.setUsername(this.username);
-            dataSource.setPassword(this.password);
-            dataSource.setDriverClassName(driver);
-            dataSource.setMaxTotal(this.maxTotal);
-            dataSource.setMaxIdle(this.maxIdle);
-            dataSource.setMaxWaitMillis(this.maxWaitMillis);
-        }
-
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.username);
+        dataSource.setPassword(this.password);
+        dataSource.setDriverClassName(driver);
+        dataSource.setMaxTotal(this.maxTotal);
+        dataSource.setMaxIdle(this.maxIdle);
+        dataSource.setMaxWaitMillis(this.maxWaitMillis);
         return dataSource;
     }
 }
